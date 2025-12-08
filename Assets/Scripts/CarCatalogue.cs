5a916f90f68978a788669706b4d72e59b20b3462
Alex Zorzella <43708160+alexzorzella@users.noreply.github.com>
2025-12-08 13:18:50 -0800

    Added model names to catalogue

--- Assets/Scripts/CarCatalogue.cs
+++ Assets/Scripts/CarCatalogue.cs
@@ -2,18 +2,18 @@ using System.Collections.Generic;
 
 public static class CarCatalogue {
     public static readonly List<CarData> cars = new() {
-        new CarData.Builder("VF120").
+        new CarData.Builder("VF120", "180sx").
             WithMaterials("Gray", "Yellow", "Purple", "Blue", "Shadow").Build(),
-        new CarData.Builder("V47").
+        new CarData.Builder("V47", "s14").
             WithCarSpecs(3070, stability: 0.5F).
             WithDriftSpecs(1.37F).
             WithMaterials("Gray", "Blue", "Crimson", "Midnight", "Dream").Build(),
-        new CarData.Builder("TZ9").
+        new CarData.Builder("TZ9", "rx7").
             WithSuspensionSpecs(0.37F).
             WithCarSpecs(3150, stability: 0.1F).
             WithDriftSpecs(1.3F).
             WithMaterials("Scuffed", "Blue", "Shadow", "Sakura", "The Original").Build(),
-        new CarData.Builder("TZ9 Grand Turismo").
+        new CarData.Builder("TZ9 Grand Turismo", "rx7").
             WithPhysicsSpecs(1400, 0.6F, 1).
             WithSuspensionSpecs(0.37F).
             WithDriftSpecs(1.3F, 1F).
@@ -24,21 +24,21 @@ public static class CarCatalogue {
             WithCarSpecs(stability: 0).
             WithDriftSpecs(1.2F).
             WithMaterials("Midnight").Build(),
-        new CarData.Builder("FD8").
+        new CarData.Builder("FD8", "hondaintegra").
             WithCarSpecs(3030, stability: 0.7F).
             WithDriftSpecs(1.4F).
             WithMaterials("Gray", "Midnight", "Crimson", "Yellow", "OJ").Build(),
-        new CarData.Builder("FD8 Grand Turismo").
+        new CarData.Builder("FD8 Grand Turismo", "hondaintegra").
             WithPhysicsSpecs(1400).
             WithSuspensionSpecs(0.3F, 0.25F, 20000, 1600).
             WithCarSpecs(3030, antiRoll: 8000).
             WithDriftSpecs(1.4F, 1F).
             WithMaterials("Gray", "Midnight", "Crimson", "Yellow", "OJ").Build(),
-        new CarData.Builder("T54").
+        new CarData.Builder("T54", "nissanskyline").
             WithCarSpecs(2850, stability: 0.9F).
             WithDriftSpecs(1, 0.45F).
             WithMaterials("Gray", "Midnight", "Crimson", "Sakura", "Shadow").Build(),
-        new CarData.Builder("T54 Grand Turismo").
+        new CarData.Builder("T54 Grand Turismo", "nissanskyline").
             WithPhysicsSpecs(1400).
             WithCarSpecs(2850, stability: 0).
             WithDriftSpecs(1, 1).
@@ -48,7 +48,7 @@ public static class CarCatalogue {
             WithCarSpecs(antiRoll: 8000, stability: 0).
             WithDriftSpecs(1.2F, 0.8F).
             WithMaterials("Beach").Build(),
-        new CarData.Builder("Tofu Machine").
+        new CarData.Builder("Tofu Machine", "ae86").
             WithCarSpecs(3050, stability: 0.25F).
             WithDriftSpecs(1.43F).
             WithMaterials("4th Stage", "Tofu", "Tofu_1", "tofu_2", "ToduHidden", "Tofu").Build(),
@@ -57,7 +57,7 @@ public static class CarCatalogue {
             WithCarSpecs(3000, 1, 5000, 0).
             WithDriftSpecs(1.2F, 0.4F).
             WithMaterials("Banana").Build(),
-        new CarData.Builder("O").
+        new CarData.Builder("O", "ae86").
             WithPhysicsSpecs(1450, 0, 1.5F).
             WithSuspensionSpecs(restHeight: 0.2F).
             WithCarSpecs(3200, antiRoll: 7000, stability: 0).
