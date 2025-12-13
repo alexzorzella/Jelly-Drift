c0b5d8e809943054369557fda9de4e4d9a187d5e
Alex Zorzella <43708160+alexzorzella@users.noreply.github.com>
2025-12-12 19:00:59 -0800

    Menu navigation works with new input system now

--- Assets/Scripts/CycleMenu.cs
+++ Assets/Scripts/CycleMenu.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -13,7 +14,9 @@ public class CycleMenu : MonoBehaviour {
     bool horizontalDone;
     int selected;
     bool verticalDone;
-    
+
+    MenuControls menuControls;
+
     void Awake() {
         selected = startSelect;
         cycles = new List<ItemCycle>();
@@ -29,14 +32,24 @@ public class CycleMenu : MonoBehaviour {
         }
 
         cycleText[selected].color = Color.black;
+
+        InitializeInput();
+    }
+
+    void InitializeInput() {
+        menuControls = new();
+
+        menuControls.User.ScrollUp.performed += ScrollUp;
+        menuControls.User.ScrollDown.performed += ScrollDown;
+
+        menuControls.User.Select.performed += Select;
+        menuControls.User.Cancel.performed += Cancel;
+
+        Debug.Log($"Input initialized as {menuControls}");
     }
 
     void Start() {
         SaveManager.Instance.state.skins[5][1] = true;
-    }
-
-    void Update() {
-        // PlayerInput();
     }
 
     void OnEnable() {
@@ -44,6 +57,12 @@ public class CycleMenu : MonoBehaviour {
         horizontalDone = true;
         verticalDone = true;
         UpdateSelected();
+
+        menuControls.Enable();
+    }
+
+    void OnDisable() {
+        menuControls.Disable();
     }
 
     void UpdateSelected() {
@@ -63,98 +82,47 @@ public class CycleMenu : MonoBehaviour {
     void ScrollUp(InputAction.CallbackContext context) {
         Navigate(new Vector2(0, 1));
     }
-    
+
     void ScrollDown(InputAction.CallbackContext context) {
         Navigate(new Vector2(0, -1));
     }
 
     void Select(InputAction.CallbackContext context) {
-            if (cycles[selected].activeCycle) {
-                // cycles[selected].Cycle(horizontalInput);
-                SoundManager.Instance.PlayCycle();
-            }
-            else {
-                SoundManager.Instance.PlayError();
-            }
-    }
-    
-    void Cancel(InputAction.CallbackContext context) {
-        cycles[backBtn].Cycle(1);
-        SoundManager.Instance.PlayCycle();
-    }
-    
-    void Navigate(Vector2 input) {
-        if (input.y != 0 && !verticalDone) {
-            cycleText[selected].color = Color.white;
-            if (correspondingText.Length != 0 && !correspondingText[selected].gameObject.CompareTag("Ignore")) {
-                correspondingText[selected].color = Color.white;
-            }
-
-            selected += (int)input.y;
-            if (selected >= cycles.Count) {
-                selected = 0;
-            }
-            else if (selected < 0) {
-                selected = cycles.Count - 1;
-            }
-
-            cycleText[selected].color = Color.black;
-            if (correspondingText.Length != 0 && !correspondingText[selected].gameObject.CompareTag("Ignore")) {
-                correspondingText[selected].color = Color.black;
-            }
-
-            SoundManager.Instance.PlayMenuNavigate();
-        }
-    }
-    
-    void PlayerInput() {
-        if (UnlockManager.Instance && UnlockManager.Instance.gameObject.activeInHierarchy) {
-            return;
-        }
-
-        int horizontalInput = (int)Input.GetAxisRaw("HorizontalMenu");
-        int verticalInput = -(int)Input.GetAxisRaw("VerticalMenu");
-        bool submit = Input.GetButtonDown("Submit");
-        bool cancel = Input.GetButtonDown("Cancel");
-        
-        if ((horizontalInput != 0 && !horizontalDone) || submit) {
-            if (cycles[selected].activeCycle) {
-                cycles[selected].Cycle(horizontalInput);
-                SoundManager.Instance.PlayCycle();
-            }
-            else {
-                SoundManager.Instance.PlayError();
-            }
-        }
-
-        if (verticalInput != 0 && !verticalDone) {
-            cycleText[selected].color = Color.white;
-            if (correspondingText.Length != 0 && !correspondingText[selected].gameObject.CompareTag("Ignore")) {
-                correspondingText[selected].color = Color.white;
-            }
-
-            selected += verticalInput;
-            if (selected >= cycles.Count) {
-                selected = 0;
-            }
-            else if (selected < 0) {
-                selected = cycles.Count - 1;
-            }
-
-            cycleText[selected].color = Color.black;
-            if (correspondingText.Length != 0 && !correspondingText[selected].gameObject.CompareTag("Ignore")) {
-                correspondingText[selected].color = Color.black;
-            }
-
-            SoundManager.Instance.PlayMenuNavigate();
-        }
-
-        if (cancel) {
-            cycles[backBtn].Cycle(1);
+        if (cycles[selected].activeCycle) {
+            cycles[selected].Cycle(1);
             SoundManager.Instance.PlayCycle();
         }
+        else {
+            SoundManager.Instance.PlayError();
+        }
+    }
 
-        horizontalDone = horizontalInput != 0;
-        verticalDone = verticalInput != 0;
+    void Cancel(InputAction.CallbackContext context) {
+        // cycles[backBtn].Cycle(1);
+        // SoundManager.Instance.PlayCycle();
+    }
+
+    void Navigate(Vector2 input) {
+        int verticalInput = -(int)input.y;
+
+        cycleText[selected].color = Color.white;
+        if (correspondingText.Length != 0 && !correspondingText[selected].gameObject.CompareTag("Ignore")) {
+            correspondingText[selected].color = Color.white;
+        }
+
+        selected += verticalInput;
+        if (selected >= cycles.Count) {
+            selected = 0;
+        }
+        else if (selected < 0) {
+            selected = cycles.Count - 1;
+        }
+
+        cycleText[selected].color = Color.black;
+        if (correspondingText.Length != 0 && !correspondingText[selected].gameObject.CompareTag("Ignore")) {
+            correspondingText[selected].color = Color.black;
+        }
+
+        SoundManager.Instance.PlayMenuNavigate();
     }
 }
