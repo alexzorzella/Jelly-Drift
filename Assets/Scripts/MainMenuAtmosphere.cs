bc28283a7a514685070a240780dad87df7e46393
Alex Zorzella <43708160+alexzorzella@users.noreply.github.com>
2025-12-13 17:22:35 -0800

    Added ocean sound in main menu

---
+++ Assets/Scripts/MainMenuAtmosphere.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Reflection;
+using UnityEngine;
+
+public class MainMenuAtmosphere : MonoBehaviour {
+    MultiAudioSource ocean;
+    MultiAudioSource gulls;
+    
+    const float gullsMinDelay = 2F;
+    const float gullsMaxDelay = 10F;
+    
+    float currentGullDelay = 0;
+    
+    void Start() {
+        InitializeAudio();
+    }
+
+    void InitializeAudio() {
+        ocean = MultiAudioSource.FromResource(gameObject, "oceanside", loop: true);
+        ocean.SetVolume(0.75F);
+        ocean.PlayRoundRobin();
+
+        // gulls = MultiAudioSource.FromResources(gameObject, "gulls", 2);
+    }
+
+    void Update() {
+        // GullSounds();
+    }
+
+    void ResetGullDelay() {
+        currentGullDelay = UnityEngine.Random.Range(gullsMinDelay, gullsMaxDelay);
+    }
+
+    void GullSounds() {
+        if (currentGullDelay <= 0) {
+            gulls.PlayRandom();
+            ResetGullDelay();
+        } else {
+            currentGullDelay -= Time.deltaTime;
+        }
+    }
+}
